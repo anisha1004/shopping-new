7349312dc81388237bd6caeabea3d39640dc32c4
anisha1004 <54628217+anisha1004@users.noreply.github.com>
2021-12-13 00:01:34 +0530

    checkout-right

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,6 +2,9 @@ import React from "react";
 import CheckoutImg from "../images/checkout-page.svg";
 import "../css/Checkout.css";
 import Confirm from "../images/confirm.svg";
+import Dhl from "../images/dhl.svg";
+import Delivery from "../images/delivery.svg";
+import Line from "../images/checkout-line.svg";
 
 const Checkout = () => {
 	return (
@@ -20,7 +23,57 @@ const Checkout = () => {
 					</div>
 					<button className='go-home'>GO TO HOMEPAGE</button>
 				</div>
-				<div className='check-right'></div>
+				<div className='check-right'>
+					<div className='delivery'>
+						<img src={Delivery} />
+						<img src={Dhl} />
+					</div>
+					<div className='final-item-list'>
+						<div className='final-item'>
+							<div className='item-name'>Way Kambas Mini Ebony</div>
+							<div className='item-quantity'>2 x INR 1.024.000</div>
+						</div>
+						<div className='final-item'>
+							<div className='item-name'>Way Kambas Mini Ebony</div>
+							<div className='item-quantity'>2 x INR 1.024.000</div>
+						</div>
+					</div>
+					{/* <div className='three'>
+						<span>Subtotal</span>
+						<span>Rs 2000</span>
+					</div>
+					<div className='three'>
+						<span>Shipping Cost</span>
+						<span>Rs 25000</span>
+					</div>
+					<div className='three'>
+						<span>Packaging</span>
+						<span>Rs 2000</span>
+					</div> */}
+					<div className='final-total'>
+						<div className='three'>
+							<span>Subtotal</span>
+							<span>Shipping Cost</span>
+							<span>Packaging</span>
+						</div>
+						<div className='three'>
+							<span>Rs 2000</span>
+							<span>Rs 25000</span>
+							<span>Rs 2000</span>
+						</div>
+					</div>
+
+					<img src={Line} />
+					<div className='grand'>
+						<span className='grandtotal'>Grand Total</span>{" "}
+						<span className='grandprice'>Rs 2700</span>
+					</div>
+					<img src={Line} />
+					<div className='address'>
+						<span>Address</span>
+						<span>18 Meera Road, Mumbai</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
